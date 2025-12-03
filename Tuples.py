1a2d8f6c75d0f58e8c16e4f252b1688ebd616976
sujal-ec <sujalraina054@gmail.com>
2025-12-03 22:02:41 +0530

    Add files via upload

---
+++ Tuples.py
@@ -0,0 +1,10 @@
+T=('apple','banana','cherry','mango','grape','orange')
+print('\n Created Tuple is:',T)
+print('\n Second Fruit is:',T[1])
+print('\n From 3-6 Fruits are:',T[3:6])
+print('\n List of all items in Tuple is:')
+for x in T:
+    print(x)
+if 'apple' in T :
+    print('\n Yes, apple is in the fruits tuple')
+print('\n Length of Tuple is:',len(T))
